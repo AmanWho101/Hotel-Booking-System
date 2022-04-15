4915fe655ee13430005961b7d243637a3b71ea37
AmanWho101 <Aman>
2022-04-15 10:50:48 +0300

    database update

--- sql/hotel.sql
+++ sql/hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 14, 2022 at 07:03 AM
+-- Generation Time: Apr 15, 2022 at 07:49 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -25,16 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `adminid` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(100) NOT NULL,
+  `lname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` int(100) NOT NULL,
+  PRIMARY KEY (`adminid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `comment`
 --
 
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
-  `comentid` int(34) NOT NULL AUTO_INCREMENT,
-  `customerid` int(34) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `message` varchar(4000) NOT NULL,
+  `comentid` int(11) NOT NULL AUTO_INCREMENT,
+  `customerid` int(11) NOT NULL,
+  `message` text NOT NULL,
   PRIMARY KEY (`comentid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
@@ -47,11 +61,15 @@ CREATE TABLE IF NOT EXISTS `comment` (
 DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
   `customerid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
+  `replyid` int(11) NOT NULL,
+  `recepid` int(11) NOT NULL,
+  `fname` varchar(200) NOT NULL,
+  `lname` varchar(100) NOT NULL,
   `country` varchar(100) NOT NULL,
   `city` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `address` varchar(200) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
   PRIMARY KEY (`customerid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
@@ -63,11 +81,46 @@ CREATE TABLE IF NOT EXISTS `customer` (
 
 DROP TABLE IF EXISTS `ratting`;
 CREATE TABLE IF NOT EXISTS `ratting` (
-  `rattingid` int(50) NOT NULL AUTO_INCREMENT,
-  `customerid` int(55) NOT NULL,
-  `rattingNo` int(55) NOT NULL,
-  `tottalNo` int(55) NOT NULL,
+  `rattingid` int(11) NOT NULL AUTO_INCREMENT,
+  `customerid` int(11) NOT NULL,
+  `rattingNo` int(50) NOT NULL,
   PRIMARY KEY (`rattingid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reception`
+--
+
+DROP TABLE IF EXISTS `reception`;
+CREATE TABLE IF NOT EXISTS `reception` (
+  `recepid` int(11) NOT NULL AUTO_INCREMENT,
+  `adminid` int(11) NOT NULL,
+  `fname` varchar(100) NOT NULL,
+  `lname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`recepid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reply`
+--
+
+DROP TABLE IF EXISTS `reply`;
+CREATE TABLE IF NOT EXISTS `reply` (
+  `replyid` int(11) NOT NULL AUTO_INCREMENT,
+  `comentid` int(11) NOT NULL,
+  `recepid` int(11) NOT NULL,
+  `adminid` int(11) NOT NULL,
+  `replymessage` text NOT NULL,
+  PRIMARY KEY (`replyid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -79,12 +132,12 @@ CREATE TABLE IF NOT EXISTS `ratting` (
 DROP TABLE IF EXISTS `reserved`;
 CREATE TABLE IF NOT EXISTS `reserved` (
   `reservationid` int(11) NOT NULL,
-  `customerid` int(34) NOT NULL,
-  `name` varchar(200) NOT NULL,
+  `customerid` int(11) NOT NULL,
+  `recepid` int(11) NOT NULL,
   `arrival` varchar(300) NOT NULL,
   `departure` varchar(300) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `room` int(10) NOT NULL
+  `room` int(50) NOT NULL,
+  `paid` int(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -95,12 +148,13 @@ CREATE TABLE IF NOT EXISTS `reserved` (
 
 DROP TABLE IF EXISTS `room`;
 CREATE TABLE IF NOT EXISTS `room` (
-  `reservationid` int(11) NOT NULL AUTO_INCREMENT,
-  `roomtypeid` int(34) NOT NULL,
-  `roomNo` int(50) NOT NULL,
-  `roomFloor` int(50) NOT NULL,
-  `roomPrice` int(50) NOT NULL,
-  PRIMARY KEY (`reservationid`)
+  `roomid` int(11) NOT NULL AUTO_INCREMENT,
+  `adminid` int(11) NOT NULL,
+  `roomtypeid` int(11) NOT NULL,
+  `roomNo` int(11) NOT NULL,
+  `roomFloor` int(11) NOT NULL,
+  `roomPrice` int(11) NOT NULL,
+  PRIMARY KEY (`roomid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -112,25 +166,9 @@ CREATE TABLE IF NOT EXISTS `room` (
 DROP TABLE IF EXISTS `roomtype`;
 CREATE TABLE IF NOT EXISTS `roomtype` (
   `typeid` int(11) NOT NULL AUTO_INCREMENT,
+  `adminid` int(11) NOT NULL,
   `typename` varchar(100) NOT NULL,
   PRIMARY KEY (`typeid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `staf`
---
-
-DROP TABLE IF EXISTS `staf`;
-CREATE TABLE IF NOT EXISTS `staf` (
-  `stafid` int(34) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `address` varchar(200) NOT NULL,
-  `role` varchar(200) NOT NULL,
-  `privilege` varchar(200) NOT NULL,
-  PRIMARY KEY (`stafid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
